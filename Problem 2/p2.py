879466e16cc07cee6668c863aeb4431534df09cf
renaisuru <renaisuru@yahoo.com>
2012-08-25 17:13:15 -0500

    Created Problem 2

---
+++ Problem 2/p2.py
@@ -0,0 +1,11 @@
+import datetime
+
+start = datetime.datetime.now()
+
+limit = 4000000
+
+
+end = datetime.datetime.now()
+
+print "Time(ms): " + str(end.microsecond - start.microsecond)
+print "Answer:   " + str(x + y)
