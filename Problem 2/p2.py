2fc6554d5f9e3a22f5326ba92feaf98350026ad4
renaisuru <renaisuru@yahoo.com>
2012-08-28 15:28:28 -0500

    cleanup

--- Problem 2/p2.py
+++ Problem 2/p2.py
@@ -4,8 +4,7 @@ start = datetime.datetime.now()
 
 limit = 4000000
 
-
 end = datetime.datetime.now()
 
 print "Time(ms): " + str(end.microsecond - start.microsecond)
-print "Answer:   " + str(x + y)
+print "Answer:   " + str(limit)
